d2237c4900aa96b3e55e84f1a6c8a90f43fa5791
Gesare <gesareayienda@gmail.com>
2020-05-31 19:28:38 +0300

    Urls update

--- gramproject/settings.py
+++ gramproject/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'gramproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'gesare',
+    'PASSWORD':'ombati',
     }
 }
 
